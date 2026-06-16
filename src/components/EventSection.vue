e37cb4294c30ede11d3109ffef5f496a886b6bd5
Diego Perozo <perozodiego@gmail.com>
2026-06-16 14:22:08 -0400

    Commit inicial.

---
+++ src/components/EventSection.vue
@@ -0,0 +1,110 @@
+<script setup>
+import MidCard from './MidCard.vue';
+import Modal from './Modal.vue';
+import { VueCal } from 'vue-cal'
+import 'vue-cal/style'
+import { computed, ref, onMounted } from 'vue';
+import http from '@/services/requests';
+
+let isModalOpen = ref(false);
+function openModal() {
+    isModalOpen.value = true;
+}
+function handleClose() {
+    isModalOpen.value = false;
+}
+
+function getEvents() {
+    http.get('/events')
+        .then((response) => {
+            eventos.value = response.data.map((evento) => {
+                return {
+                    ...evento,
+                    end: new Date(evento.end_datetime),
+                    start: new Date(evento.start_datetime)
+                }
+            });
+        }).catch((error) => {
+            console.log(error.response.data.detail);
+        })
+}
+
+function getEmoji(emoji) {
+    if(emoji == 'e-musica')
+        return "🎶";
+
+    if(emoji == 'e-comida')
+        return "🍛";
+
+    if(emoji == 'e-oracion')
+        return "🛐";
+
+    if(emoji == 'e-reunion')
+        return "⛪";
+
+    return "celebration";
+}
+
+onMounted(() => {
+    getEvents();
+})
+
+//Calendario
+const eventos = ref([]);
+
+//Visible en la tarjeta
+const colors = ref(['from-primary to-deep-blue', 'from-deep-blue to-blue-400', 'from-primary/80 to-primary', 'from-gray-700 to-black']);
+
+const filterList = computed(() => {
+    let fechaActual = new Date();
+    fechaActual.setHours(0, 0, 0, 0);
+
+    return eventos.value
+        .map((f) => ({ ...f, auxDate: new Date(f.start) }))
+        .sort((a, b) => a.auxDate - b.auxDate)
+        .filter((f) => f.auxDate >= fechaActual)
+        .slice(0, 4);
+});
+
+</script>
+<template>
+    <section class="py-24 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" id="event">
+        <div class="flex items-center justify-between mb-12">
+            <div>
+                <h2 class="text-deep-blue dark:text-white text-4xl font-bold mb-2">Próximos Eventos</h2>
+                <p class="text-gray-500 dark:text-gray-400">Mantente conectado con nuestras actividades semanales.
+                </p>
+            </div>
+            <button @click="openModal"
+                class="bg-gray-100 dark:bg-gray-800 text-deep-blue dark:text-white px-6 py-3 rounded-full font-bold hover:bg-primary hover:text-white transition-all cursor-pointer">Ver
+                calendario completo</button>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <mid-card v-for="(event, index) in filterList" :key="event.title" :event="event.title" :icon="getEmoji(event.icon)"
+                :location="event.location" :tag="event.tag" :date="event.start.toLocaleString('es-Es', {
+                    weekday: 'short', // Día abreviado (lun.)
+                    month: 'short',   // Mes abreviado (feb.)
+                    day: 'numeric',   // Día del mes (22)
+                    year: 'numeric',  // Año (2026)
+                    hour: '2-digit',  // Hora (15)
+                    minute: '2-digit',// Minutos (30)
+                    hour12: false     // Formato 24h
+                }).toLocaleUpperCase()" :colors="colors[index]" />
+        </div>
+    </section>
+
+    <modal :show="isModalOpen" title="Eventos" size="w-3xl" @close="handleClose">
+        <div class="flex flex-col overflow-hidden">
+            <div class="relative">
+                <vue-cal v-model:events="eventos" :editable-events="false" view="days"
+                    :views="{ day: '', days: { cols: 5, rows: 1 }, month: '', year: '', years: '' }" locale="es"
+                    :time-from="7 * 60" :time-to="18 * 60" dark sm class="text-center" />
+            </div>
+        </div>
+    </modal>
+</template>
+<style scoped>
+.vuecal {
+    --vuecal-primary-color: #9b263e;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+import { watch } from 'vue';
+const props = defineProps({
+    show: {
+        type: Boolean,
+    },
+    title: {
+        type: String
+    },
+    size: {
+        type: String,
+        default: ''
+    },
+});
+
+const emit = defineEmits(['close']);
+
+function closeModal(){
+    emit('close');
+}
+
+watch(() => props.show, (newVal) => {
+    document.body.style.overflow = newVal ? 'hidden':'auto';
+});
+
+</script>
+
+<template>
+    <div v-show="show" class="fixed inset-0 bg-gray-900/60 flex items-center justify-center z-50 text-white">
+        <div class="bg-bg-modal p-6 rounded-lg shadow-xl min-w-125 max-w-3xl max-h-160 overflow-auto m-4 flex flex-col relative" :class="size">
+            <div class="flex items-center justify-between mb-4">
+                <h3 class="text-4xl font-semibold m-auto">{{ title }}</h3>
+                <button @click="closeModal" class="text-gray-500 hover:text-white cursor-pointer text-2xl">
+                    &times;
+                </button>
+            </div>
+
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style></style>
